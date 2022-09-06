1a2fb50cbbaa6d5cc4a947adae808bf761ce7b89
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-09-06 20:59:05 +1000

    Remove ReactiveUI stuff

--- AemulusLib/AemulusLib/Models/Loadout.cs
+++ AemulusLib/AemulusLib/Models/Loadout.cs
@@ -1,5 +1,4 @@
 ﻿using AemulusEx.Models.Enums;
-using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,23 +11,19 @@ namespace AemulusEx.Models
     /// <summary>
     /// A loadout displayed in the <see cref="Views.LoadoutView"/>
     /// </summary>
-    public class Loadout : ReactiveObject
+    public class Loadout
     {
-        private string _name;
-        public string Name { get => _name; set => this.RaiseAndSetIfChanged(ref _name, value); }
-        private string _description;
-        public string Description { get => _description; set => this.RaiseAndSetIfChanged(ref _description, value); }
-        private Game _game;
-        public Game Game { get => _game; set => this.RaiseAndSetIfChanged(ref _game, value);  }
-        public ObservableCollection<LoadoutPackage> Packages { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Game Game { get; set; }
+        public List<LoadoutPackage> Packages { get; set; }
 
         public Loadout(string name, string description, Game game, List<LoadoutPackage> packages)
         {
-            _name = name;
-            _description = description;
-            _game = game;
-            Packages = new ObservableCollection<LoadoutPackage>(packages);
-            Packages.CollectionChanged += (_, _) => this.RaisePropertyChanged("Packages");
+            Name = name;
+            Description = description;
+            Game = game;
+            Packages = packages;
         }
 
         /// <summary>
@@ -36,11 +31,10 @@ namespace AemulusEx.Models
         /// </summary>
         public Loadout()
         {
-            _name = "Default";
-            _description = "";
-            _game = Game.Persona_4_Golden;
-            Packages = new ObservableCollection<LoadoutPackage>();
-            Packages.CollectionChanged += (_, _) => this.RaisePropertyChanged("Packages");
+            Name = "Default";
+            Description = "";
+            Game = Game.Persona_4_Golden;
+            Packages = new List<LoadoutPackage>();
         }
     }
 }
